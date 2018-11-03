46baf5db6cd86ed58e585db1f5b37fa45be5ec9b
lovemercybiz <lovemercybiz@gmail.com>
2018-11-03 19:22:14 -0400

    Added: bootstrap, images, errors log, sass styles, a better products landing page, created development database, schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+Product.create!(title: 'Roses', 
+    description:
+    %{<p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. At exercitationem hic suscipit? Error necessitatibus dignissimos blanditiis quas consequuntur quam totam vel corporis illo. Autem, magni atque facilis repellendus doloremque laboriosam! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis consectetur vitae modi temporibus exercitationem sequi possimus ipsum commodi itaque provident. Quo aut dolore ratione mollitia molestias exercitationem, expedita placeat voluptate? 
+            </p>},
+    image_url: 'roses.jpg',
+    price: 29.99)
