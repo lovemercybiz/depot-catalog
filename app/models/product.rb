63202da8ff3c6fb6522d0c10dd18972c868354aa
lovemercybiz <lovemercybiz@gmail.com>
2018-11-04 10:14:26 -0500

    Added: image and validation. Updated the unit tests Rails provided

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+    validates :title, :description, :image_url, presence: true
+    validates :price, numericality: { greater_than_or_equal_to: 5} 
+    validates :title, uniqueness: true
+    validates :image_url, allow_blank: true, format: {
+        with: %r{\.(gif|jpg|png|jpeg)\Z}i,# This regex matches . (. means anything?) followed by 3 options
+        message: 'must be a GIF, JPG,JPEG, or PNG image file.'
+    }
 end
+
