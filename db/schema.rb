d3924a6ca380eb92e96c44ba270b5ad8daa55a4b
lovemercybiz <lovemercybiz@gmail.com>
2018-11-17 17:01:42 -0500

    Added scaffold Cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_03_172524) do
+ActiveRecord::Schema.define(version: 2018_11_17_215925) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
