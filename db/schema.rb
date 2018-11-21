4421a52ce350328cfd78afd816aee3ed6eaffacb
lovemercybiz <lovemercybiz@gmail.com>
2018-11-21 09:35:19 -0500

    Add up and down method, db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_135604) do
+ActiveRecord::Schema.define(version: 2018_11_21_141431) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
